d4697a5a8bd76a65be9cdbd1f91b82c081d2f98a
17matsumoto <17yuya.baseball@gmail.com>
2022-10-17 09:52:22 +0900

    課題の提出

---
+++ src/demo/ArgsLastPrinter.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class ArgsLastPrinter {
+
+	public static void main(String[] args) {
+			if(args.length == 3) {
+				System.out.println(args[2]);
+				}else 
+				System.out.println("なし");
+	}
+}
